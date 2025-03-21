79e82e5d9ba7b357d19bc8b1ef759ed85ca3a858
longy <longhanzhang999@gmail.com>
2025-03-21 21:26:53 +1000

    day 1 commit restore

---
+++ variable.tf
@@ -0,0 +1,25 @@
+
+variable "aws_region" {
+  type        = string
+  default     = "us-east-1"
+  description = "my default region"
+}
+
+
+variable "vm_name" {
+  type    = string
+  default = "my_linux"
+
+}
+
+variable "vpc_cidr" {
+  type        = string
+  default     = "10.0.0.0/16"
+  description = "this cidr block is for the vpc"
+}
+
+variable "subnet_cidr" {
+  type        = string
+  default     = "10.0.1.0/24"
+  description = "this cidr block is for the subnet"
+}
